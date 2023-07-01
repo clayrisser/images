b690c3a8ad7eccea7cca11a4063c92029447d5f6
Clay Risser <clayrisser@gmail.com>
2023-07-01 04:18:02 -0500

    ♻️ refactor: Update bootstrap.mk with new version and size
    This commit updates the ".mkpm/.bootstrap.mk" file with a new version and size. The SHA256 hash has been changed to "652cb9615a491eb42827e863b4b69cce4f39de90872f4a73297c98eda83c37be", and the size is now 16453.

--- .mkpm/.bootstrap.mk
+++ .mkpm/.bootstrap.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0634d4729681f01e488a4680b048e9638c5e6e0ad4d13da2a60a03987dbce849
-size 16423
+oid sha256:652cb9615a491eb42827e863b4b69cce4f39de90872f4a73297c98eda83c37be
+size 16453
