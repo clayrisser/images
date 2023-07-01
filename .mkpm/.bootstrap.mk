eb8af3d072a5bd0527b3b4257a7e205330c08611
Clay Risser <clayrisser@gmail.com>
2023-07-01 04:29:07 -0500

    🚑 fix: Update checksum and size in .bootstrap.mk
    The checksum and size values in the `.bootstrap.mk` file have been updated. The new checksum is `0a1fce4e98bb422d0ae5e8e43175c113fcf0f4a19fb6da436403d72915699a35`, and the size is now 16480.

--- .mkpm/.bootstrap.mk
+++ .mkpm/.bootstrap.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:652cb9615a491eb42827e863b4b69cce4f39de90872f4a73297c98eda83c37be
-size 16453
+oid sha256:0a1fce4e98bb422d0ae5e8e43175c113fcf0f4a19fb6da436403d72915699a35
+size 16480
