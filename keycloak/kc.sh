8422ea704cdc2ea6585753cf3023739402dbc02b
Clay Risser <clayrisser@gmail.com>
2023-08-06 02:58:54 -0500

    ✨ feat: Add .envrc, default.nix, and Dockerfile for Keycloak
    This commit adds the following files for Keycloak:
    - Added .envrc file
    - Created default.nix file with package dependencies
    - Added Dockerfile with environment variables and build instructions
    
    The changes in detail:
    - Added .envrc file with "use nix" content
    - Created default.nix file with package dependencies and buildInputs
    - Updated Dockerfile, setting environment variables KC_HTTPS_KEY_STORE_FILE and KC_HTTPS_KEY_STORE_PASSWORD
    - Modified kc.sh script to check if KEYCLOAK_IMPORT is provided and if the file exists before importing it
    
    These changes provide necessary configurations and dependencies for Keycloak deployment.

--- keycloak/kc.sh
+++ keycloak/kc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IMPORT_FLAG_FILE=/opt/keycloak/imported.flag
-if [ "$KEYCLOAK_IMPORT" != "" ]; then
+if [ "$KEYCLOAK_IMPORT" != "" ] && [ -f "$KEYCLOAK_IMPORT" ]; then
     if [ ! -f "$IMPORT_FLAG_FILE" ]; then
         /opt/keycloak/bin/_kc.sh import --file "$KEYCLOAK_IMPORT" --override=false
         touch "$IMPORT_FLAG_FILE"
